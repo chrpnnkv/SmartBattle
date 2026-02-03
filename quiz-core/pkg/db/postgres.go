9f208fe040868fc1a9d6254b545fad6a92b1d29a
daniil <marhind_1412@mail.ru>
2026-02-03 10:38:42 +0300

    feat(db): add postgres connection and db helpers

---
+++ quiz-core/pkg/db/postgres.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(url string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	return db
+}
